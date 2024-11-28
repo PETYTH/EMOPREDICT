aaa47b744524f82adc16ba0cbf6d1ccad7564ce9
Prince PETYTH <77441840+PETYTH@users.noreply.github.com>
2024-11-28 11:53:45 +0100

    Delete Backend directory

--- Backend/models/Emotion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const emotionSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    emotion: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Emotion', emotionSchema);
